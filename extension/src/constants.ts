b5aa220933a00081c2ae62df8d9ba126e35bca65
Roman Radchenko <nodenwwsfww@gmail.com>
2025-04-04 10:40:03 +0300

    [extension]: Add IGNORE_PATTERNS constant and consolidate all_urls functionality

---
+++ extension/src/constants.ts
@@ -0,0 +1,9 @@
+export const IGNORE_PATTERNS = [
+    'googleads.g.doubleclick.net',
+    'googletagmanager.com',
+    'googletag.com',
+    'googletag.com',
+    'pornhub.com',
+    'xvideos.com',
+    'xnxx.com',
+]
